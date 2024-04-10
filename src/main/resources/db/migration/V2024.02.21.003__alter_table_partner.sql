4373eb13325b0e767963fd99998a9ce75515870a
alemagno10 <alemagno03@gmail.com>
2024-04-10 10:11:39 -0300

    fix: db

--- src/main/resources/db/migration/V2024.02.21.003__alter_table_partner.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE partnerDB
-    ADD COLUMN tx_cnpj VARCHAR(14);
-
-ALTER TABLE partnerDB
-    DROP COLUMN tx_hash;
--- src/main/resources/db/migration/V2024.02.21.003__rename_partner.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE partnerDB
-    RENAME COLUMN id_account TO id_partner;
-
-ALTER TABLE partnerDB
-    DROP CONSTRAINT account_pkey;
-
-ALTER TABLE partnerDB
-    ADD CONSTRAINT partner_pkey PRIMARY KEY (id_partner);
