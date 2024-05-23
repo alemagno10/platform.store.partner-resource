851b87ea8bd5d76e75377eb2772e0c1fe12cd4da
alemagno10 <alemagno03@gmail.com>
2024-05-23 14:33:14 -0300

    feat: redis

--- src/main/java/insper/store/partner/PartnerService.java
+++ src/main/java/insper/store/partner/PartnerService.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import lombok.NonNull;
@@ -14,26 +17,31 @@ public class PartnerService {
     @Autowired
     private PartnerRepository partnerRepository;
 
+    @CachePut(value = "partner", key = "#results.id")
     public Partner create(Partner in) {
         return partnerRepository.save(new PartnerModel(in)).to();
     }
 
+    @Cacheable(value = "partner", key = "#id")
     public Partner read(@NonNull String id) {
         return partnerRepository.findById(id).map(PartnerModel::to).orElseThrow(() -> new IllegalArgumentException("Partner not found"));
     }
 
+    @Cacheable(value = "allpartners")
     public List<PartnerModel> findAll() {
         List<PartnerModel> list = new ArrayList<>();
         partnerRepository.findAll().forEach(list::add);
         return list;
     }
 
+    @CachePut(value = "partner", key = "#id")
     public Partner update(@NonNull String id, PartnerIn in) {
         PartnerModel partner = partnerRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Partner not found"));
         partner.update(in);
         return partnerRepository.save(partner).to();
     }
 
+    @CacheEvict(value = "partner", key = "#id")
     public void delete(@NonNull String id) {
         if(partnerRepository.existsById(id)){
             partnerRepository.deleteById(id);
